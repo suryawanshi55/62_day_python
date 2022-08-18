11e5b0517630f751d8e3888f0162ef921c7290a2
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-08-18 21:35:50 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,8 @@
+# class number:
+#     x=2022
+#     y=365
+# print(number.x)               # 2022
+# print(number.y)              # 365
+# b=number()
+# print(b.x)                     #2022
+# print(b.y)                    # 365
